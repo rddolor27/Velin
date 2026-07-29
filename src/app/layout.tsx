c991793114f6f363bc51e44bb457a4f15f389c76
rddolor27 <rddolor22@gmail.com>
2026-07-29 13:24:56 +0800

    feat: default to dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
